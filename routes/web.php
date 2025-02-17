83e304c8257152037887bf3048261d0b73c8648f
Hariharan Ravi <hariravidhivya@gmail.com>
2025-02-17 07:01:09 +0000

    Revert "Added controller, routes, and basic views"
    This reverts commit 9013372937dcf0c80f54b41c9a303300dae21327.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,4 +16,3 @@ use App\Http\Controllers\ContactController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('contacts', ContactController::class);
